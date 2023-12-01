e7ef8d0cda922aaa2efe64024b0cf57356012dd2
Patrik <patrikvonmalottki@proton.me>
2023-12-02 00:21:56 +0100

    On branch main Initial commit 	new file:   .gitignore 	new file:   01/.gitignore 	new file:   01/calibration_document.txt     new file:   01/puzzle_01_1.py

---
+++ 01/puzzle_01_1.py
@@ -0,0 +1,18 @@
+calibration_document = open("/home/paddi/Documents/projekte/python projects/adven_of_code/01/calibration_document.txt", "r")
+data = calibration_document.read()
+result = 0
+for line in data.split('\n'):
+    cache = []
+    for character in range(0, len(line)):
+        if line[character].isnumeric():
+            x = int(line[character])
+            cache.append(x)
+        else:
+            pass
+    first_last = [cache[n] for n in (0, -1)]
+    number = ''
+    for i in first_last:
+        number += '' + str(i)
+    result = result + int(number)
+print(result)
+calibration_document.close() 

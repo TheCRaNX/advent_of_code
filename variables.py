b2be14c0a8d0edbf8bde4132a89230460f99afd8
Patrik <patrikvonmalottki@proton.me>
2023-12-02 15:30:22 +0100

    modified:   .gitignore 	modified:   01/puzzle_02.py 	deleted:    variables.py

--- variables.py
+++
@@ -1,2 +0,0 @@
-link_to_calibration_document = '/home/paddi/Documents/projekte/python projects/adven_of_code/01/calibration_document.txt'
-test = '/home/paddi/Documents/projekte/python projects/adven_of_code/01/test.txt'

ef96fb8d90035148fb8555cf73a863782f17c0e7
Patrik <patrikvonmalottki@proton.me>
2023-12-02 21:09:01 +0100

    deleted:    01/test.txt 	deleted:    01/variables.py 	modified:   2023/01/puzzle_02.py

--- 01/variables.py
+++
@@ -1,2 +0,0 @@
-test = '/home/paddi/Documents/projekte/python projects/adven_of_code/01/test.txt'
-link_to_calibration_document = '01/calibration_document.txt'
--- 2023/01/puzzle_02.py
+++ 2023/01/puzzle_02.py
@@ -1,8 +1,6 @@
 from variables import *
-
 help_dict = {'one': '1','two': '2','three': '3','four': '4','five': '5','six': '6','seven': '7','eight': '8','nine': '9','zero': '0'}
 result = 0
-
 calibration_document = open(link_to_calibration_document, "r")
 data = calibration_document.read()
 for line in data.split('\n'):

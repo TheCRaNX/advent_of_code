5baa5e0bc223c0730b821790a2d602736e8fe324
Patrik <patrikvonmalottki@proton.me>
2023-12-06 23:38:19 +0100

    modified:   2023/04/copy.txt 	modified:   2023/04/puzzle_04_02.py

--- 2023/04/puzzle_04_02.py
+++ 2023/04/puzzle_04_02.py
@@ -31,6 +31,27 @@ def check_number(p_number_card, p_winning_numbers):
 def calc_and_save_win_amount(p_winning_number):
     p_win_amount = p_winning_number * 2
 
+def count_matches(p_number_list, p_winners_list):
+    l_win_count = 0
+    for i in p_number_list:
+        if check_number(i, p_winners_list) == True:
+            l_win_count = l_win_count + 1
+        else:
+            #print("no luck")
+            pass
+    #print(l_win_count)
+    return(l_win_count)
+
+def add_match_to_dict(p_winners_list, p_match_dict):
+    for i in p_winners_list:
+        #print("---------MATCH AT------------" + str(i))
+        #print("OLD: " + str(p_match_dict[i]))
+        p_match_dict[i] = p_match_dict[i] + 1
+        #print("NEW: " + str(p_match_dict[i]))
+        #p_cache_list.append(i)
+card_matches = {}
+
+
 #DELETE CONTENT OF COPY.TXT#
 open(copy_txt, 'w').close()
 
@@ -38,11 +59,23 @@ games = 0
 calibration_document = open(test, "r")
 data = calibration_document.read()
 result = 0
+
+for line in data.split('\n'):
+    games = games + 1
+    card_matches[games] = 0
+
+print(card_matches)
+
+games = 0
+
 for line in data.split('\n'):
     split_line = line.split()
     cache_card_numbers = []
     cache_winning_numbers = []
     games = games + 1
+    
+    winning_cards = []
+    
     #print("Games played " + str(games))
     
     string_cards = create_substring(line, get_line_number(line, ':'), get_line_number(line, '|')).split()
@@ -55,16 +88,25 @@ for line in data.split('\n'):
     for word in string_winning:
         if word.isnumeric():
             cache_winning_numbers.append(word)
-
-    for i in cache_card_numbers:
-        if check_number(i, cache_winning_numbers) == True:
-            print(games)
-            write_to_text(copy_txt, line)
-            #print(get_certain_line_of_txt_file(link_to_puzzle_input)[int(i) - 1])
-            write_to_text(copy_txt, get_certain_line_of_txt_file(link_to_puzzle_input)[int(i) - 1])
             
-        else:
-            pass
+    #print(cache_card_numbers, cache_winning_numbers)
+    matches = count_matches(cache_card_numbers, cache_winning_numbers)
+    if matches == 0:
+        break
+    
+    l_games = games
+    for i in range(0, matches):
+        l_games = l_games + 1
+        #print(games)
+        winning_cards.append(l_games)
+    
+    print(winning_cards)
+    #print(cache_winning_cards)
+    
+    add_match_to_dict(winning_cards, card_matches)
+    
+    cache_winning_cards = winning_cards
+    
     '''
     print(cache_card_numbers)
     print("######")
@@ -75,7 +117,7 @@ for line in data.split('\n'):
 
     
 calibration_document.close() 
-
+print(card_matches)
 exit()
 
 calibration_document = open(copy_txt, "r")

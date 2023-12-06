5ba2ed5488f3519b5387e0f3d36a6e9ae0bf9b11
Patrik <patrikvonmalottki@proton.me>
2023-12-06 22:04:47 +0100

    modified:   2023/06/puzzle_06_01.py

--- 2023/06/puzzle_06_01.py
+++ 2023/06/puzzle_06_01.py
@@ -1,34 +1,12 @@
 import linecache
 import re
-
 from variables import *
-import re
-
-
-def get_line_number(p_line, p_char):
-    l_line_number = p_line.find(p_char)
-    return(l_line_number)
-
-def get_line(p_line_number):
-    l_line = linecache.getline(test, p_line_number)
-    return l_line
-
-def create_substring(p_string, p_string_start, p_string_end):
-    l_substring = p_string[p_string_start:p_string_end]
-    return(l_substring)
-
-def calc_speed(p_distance, p_time):
-    l_speed = int(p_distance) / int(p_time)
-    print("The speed is " + str(l_speed) + "mm/ms")
-    return(l_speed)
-
-
 
 cache_time = []
 cache_recordDistance = []
 time_recordDistance = {}
 
-calibration_document = open(test, "r")
+calibration_document = open(link_to_puzzle_input, "r")
 data = calibration_document.read()
 line_count = 1
 for line in data.split('\n'):
@@ -41,16 +19,21 @@ for line in data.split('\n'):
                 cache_recordDistance.append(word)
         else:
             pass
-
     line_count = line_count + 1
 calibration_document.close()
 
 for (i, j) in zip(cache_time, cache_recordDistance):
-    time_recordDistance[i] = j
+    if i in time_recordDistance:
+        time_recordDistance[str(i) + "."] = j    
+    else:    
+        time_recordDistance[i] = j
 
-print(time_recordDistance)
-
+result = 1
 for key in time_recordDistance:
-    for i in range(0, int(key)):
-        print(i + 1)
-        print(time_recordDistance[key])
+    cache_result = 0
+    for i in range(0, int(key.replace(".", ""))):
+        remaining_time = int(key.replace(".", "")) - i
+        if (remaining_time * int(i)) > int(time_recordDistance[key]):
+            cache_result = cache_result + 1
+    result = result * cache_result
+print(result)

106d4d588677cf9df6e15562ec2d4e15e2e177f4
Patrik <patrikvonmalottki@proton.me>
2023-12-06 21:13:37 +0100

    modified:   2023/06/variables.py

--- 2023/06/variables.py
+++ 2023/06/variables.py
@@ -1,3 +1,3 @@
-link_to_puzzle_input = '/home/paddi/Documents/projekte/python projects/advent_of_code/2023/06/puzzle_input.txt'
-copy_txt = '/home/paddi/Documents/projekte/python projects/advent_of_code/2023/06/copy.txt'
-test = '/home/paddi/Documents/projekte/python projects/advent_of_code/2023/06/test.txt'
+link_to_puzzle_input = '/home/paddi/Documents/projekte/python projects/adven_of_code/2023/06/puzzle_input.txt'
+copy_txt = '/home/paddi/Documents/projekte/python projects/adven_of_code/2023/06/copy.txt'
+test = '/home/paddi/Documents/projekte/python projects/adven_of_code/2023/06/test.txt'

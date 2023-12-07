239222ca295a462b82f2d1bac1e08894f6a86e24
Patrik <patrikvonmalottki@proton.me>
2023-12-07 20:13:04 +0100

    modified:   2023/02/puzzle_02_01.py 	modified:   2023/02/variables.py 	modified:   2023/04/variables.py 	modified:   2023/06/variables.py

--- 2023/02/puzzle_02_01.py
+++ 2023/02/puzzle_02_01.py
@@ -1,114 +1,110 @@
-import collections
 import itertools
-import linecache
 import re
 from variables import *
-calibration_document = open(test, "r")
 
+#VARIABLES#
+highest_number = 0
 game_count = 0
 red_cubes = 0
 green_cubes = 0
 blue_cubes = 0
 cube_colors = ["red", "green", "blue"]
-bag = {'red_cubes':12,'green_cubes':13,'blue_cubes':14}
+bag = {'red':12,'green':13,'blue':14}
+result = 0
 
-
-#DICTIONARY VARIABLES#
-red_cube = []
-green_cube = []
-blue_cube = []
-
-#DICTIONARIES#
-
-games = {}
-games[1] = {}
-
-
-
-
-data = calibration_document.read()
-
+#FUNCTIONS#
 def get_ball_position(p_string, p_list):
     l_ball_positions = {}
     for item in p_list:
-        #print(item)
         l_pattern = re.compile(f'(?i){re.escape(item)}')
         l_positions = [match.start() for match in l_pattern.finditer(p_string)]
-        #print(l_positions)
         l_ball_positions[item] = l_positions
     return(l_ball_positions)
 
-def get_line(p_line_number):
-    l_line_number = linecache.getline(test, p_line_number)
-    return l_line_number
-
-def create_substring(p_string, p_string_start):
-    l_sub_string = p_string[p_string_start - 2:p_string_start]
-    return(l_sub_string)
+def create_substring(p_string, p_string_end, p_return):
+    l_string_start = p_string_end - p_return - 1
+    l_sub_string = p_string[l_string_start:p_string_end]
+    l_sub_string_cleaned = l_sub_string.replace(" ", "")
+    return(l_sub_string_cleaned)
 
 def get_ball_amount(p_string, p_list):
     get_ball_position(p_string, p_list)
 
-
-rounds_counter = 0
-for line in data.split('\n'):
-    rounds_counter = 1
-    game_count = game_count + 1
-    #print("NEW GAME!")
-    #print("Game: " + str(game_count) + " and Round: " + str(rounds_counter))
-    #print(game_count)
-    #print(line)
-    for letter in line:
-        if letter == ";":
-            rounds_counter = rounds_counter + 1
-            #print("Game: " + str(game_count) + " and Round: " + str(rounds_counter))
-            
-#print(games)
-
-game_count = 0
+#START#
+calibration_document = open(link_to_puzzle_input, "r")
+data = calibration_document.read()
 
 for line in data.split('\n'):
+    cache_highest_number = 0
+    for word in line.split():
+        for letter in line:
+            if letter == ":":
+                f_round_start = 1
+        if f_round_start == 1:
+             if word.isnumeric():
+                  cache_highest_number = int(word)
+                  if highest_number < cache_highest_number:
+                       highest_number = cache_highest_number
+
+longest_number = len(str(highest_number))
+
+for line in data.split('\n'):
+    cache_game_cubes = {}
     game_count = game_count + 1
-    print(game_count)
-    #print(line)
     for letter in line:
         if letter.isnumeric():
-            #print(letter)
             pass
         if str(letter) == str(":"):
             break
-
-    #print(get_ball_position(line, cube_colors))
-    #print(get_ball_amount(line, cube_colors))
-    #for key in get_ball_position(line, cube_colors):
-     #   print(key)
-      #  key[0]
         
     pos_dict = get_ball_position(line, cube_colors)
-    
-    for key, value in ( 
-        itertools.chain.from_iterable( 
-            [itertools.product((k, ), v) for k, v in pos_dict.items()])): 
-                print(key, create_substring(line, value))
-                amount = key, create_substring(line, value)
+    highest_red_cube = 0
+    highest_green_cube = 0
+    highest_blue_cube = 0
+    for key, value in (
+         itertools.chain.from_iterable( 
+            [itertools.product((k, ), v) for k, v in pos_dict.items()])):
+                amount = key, create_substring(line, value, longest_number)
                 if key == "red":
-                    print("its red" + str(amount))
-                    red_cube.append(amount[1])
                     red_cubes = red_cubes + int(amount[1])
+                    cache_game_cubes[key] = red_cubes
+                     
+                    cache_highest_red_cube = int(amount[1])
+                    if highest_red_cube < cache_highest_red_cube:
+                         highest_red_cube = cache_highest_red_cube                 
                     
                 if key == "green":
-                    print("its green" + str(amount))
-                    #green_cubes = green_cubes + int(amount)
-                if key == "blue":
-                    print("its blue" + str(amount))
-                    #blue_cubes = blue_cubes + int(amount)
-                    
-    print(str(red_cubes) + " red cubes in total")
-    red_cubes = 0
-            
-        
-    
+                    green_cubes = green_cubes + int(amount[1])
+                    cache_game_cubes[key] = green_cubes
+                    cache_highest_green_cube = int(amount[1])
+                    if highest_green_cube < cache_highest_green_cube:
+                         highest_green_cube = cache_highest_green_cube  
 
-    #print(position_amount - 2)
-calibration_document.close() 
-print(red_cubes)
+                if key == "blue":
+                    blue_cubes = blue_cubes + int(amount[1])
+                    cache_game_cubes[key] = blue_cubes                     
+                    cache_highest_blue_cube = int(amount[1])
+                    if highest_blue_cube < cache_highest_blue_cube:
+                         highest_blue_cube = cache_highest_blue_cube    
+
+    yes = 0
+    
+    if int(bag["red"]) >=  int(highest_red_cube):
+        yes = yes + 1
+
+    if int(bag["green"]) >=  int(highest_green_cube):
+        yes = yes + 1
+
+    if int(bag["blue"]) >=  int(highest_blue_cube):
+        yes = yes + 1
+
+    if yes == 3:
+        result = result + game_count
+
+    yes = 0
+    red_cubes = 0
+    green_cubes = 0
+    blue_cubes = 0
+    
+calibration_document.close()
+print(result)
--- 2023/04/variables.py
+++ 2023/04/variables.py
@@ -1,3 +1,3 @@
-link_to_puzzle_input = '/home/paddi/Documents/projekte/python projects/adven_of_code/2023/04/puzzle_input.txt'
-copy_txt = '/home/paddi/Documents/projekte/python projects/adven_of_code/2023/04/copy.txt'
-test = '/home/paddi/Documents/projekte/python projects/adven_of_code/2023/04/test.txt'
+link_to_puzzle_input = '/home/paddi/Documents/projekte/python projects/advent_of_code/2023/04/puzzle_input.txt'
+copy_txt = '/home/paddi/Documents/projekte/python projects/advent_of_code/2023/04/copy.txt'
+test = '/home/paddi/Documents/projekte/python projects/advent_of_code/2023/04/test.txt'
